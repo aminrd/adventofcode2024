994bb36f5ea318c92f73a291bd47b9d4a9d42e5e
Amin <amin.rad.1371@gmail.com>
2024-12-04 21:24:21 -0800

    Day 05 part two

--- day05.py
+++ day05.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+from functools import cmp_to_key
 
 DEBUG = sys.gettrace() is not None
 input_file = "inputs/test.txt" if DEBUG else "./inputs/day05.txt"
@@ -16,8 +17,17 @@ while "|" in lines[0]:
     left[l].add(r)
     right[r].add(l)
 
+
+# Left[X] contains all values X is smaller
+def comparator(a, b):
+    if b in left[a]:
+        return -1
+    elif a in left[b]:
+        return 1
+    return 0
+
 class Ordering:
-    def __init__(self, line: str):
+    def __init__(self, line):
         self.arr = list(map(int, line.split(',')))
         self.n = len(self.arr)
 
@@ -26,6 +36,10 @@ class Ordering:
             if any(self.arr[j] not in left[val] for j in range(i+1, self.n)):
                 return False
         return True
+
+    def fix(self):
+        new_arr = list(sorted(self.arr, key=cmp_to_key(comparator)))
+        return Ordering(",".join(str(val) for val in new_arr))
 
     def get_middle(self):
         return self.arr[self.n // 2]
@@ -39,6 +53,12 @@ for line in lines:
 part_one = sum(ordering.get_middle() for ordering in orderings if ordering.is_valid())
 print(f"Part one = {part_one}")
 
-part_two = None
+part_two = 0
+for ordering in orderings:
+    if ordering.is_valid():
+        continue
+
+    part_two += ordering.fix().get_middle()
+
 print(f"Part two = {part_two}")
 
