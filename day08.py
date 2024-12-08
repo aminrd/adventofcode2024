286e7aab198ceb76b4d9f3e41048032a3405282e
Amin <amin.rad.1371@gmail.com>
2024-12-07 21:32:08 -0800

    Solve day 08 part two

--- day08.py
+++ day08.py
@@ -24,6 +24,26 @@ def get_valid_antinodes(loc1: tuple, loc2: tuple):
     return [(x, y) for (x, y) in output if valid(x, y)]
 
 
+def get_valid_antinodes_many(loc1: tuple, loc2: tuple):
+    x1, y1 = loc1
+    x2, y2 = loc2
+    diff_x = x1 - x2
+    diff_y = y1 - y2
+
+    output = []
+    while valid(x1, y1):
+        output.append((x1, y1))
+        x1 += diff_x
+        y1 += diff_y
+
+    while valid(x2, y2):
+        output.append((x2, y2))
+        x2 -= diff_x
+        y2 -= diff_y
+
+    return output
+
+
 for i, row in enumerate(grid):
     for j, val in enumerate(row):
         if val != '.':
@@ -35,12 +55,22 @@ for arr in freq.values():
         continue
 
     for i, loc1 in enumerate(arr):
-        for loc2 in arr[i+1:]:
+        for loc2 in arr[i + 1:]:
             for (x, y) in get_valid_antinodes(loc1, loc2):
                 antinodes.add((x, y))
 
 part_one = len(antinodes)
 print(f"Part one = {part_one}")
 
-part_two = None
+antinodes2 = set()
+for arr in freq.values():
+    if len(arr) < 2:
+        continue
+
+    for i, loc1 in enumerate(arr):
+        for loc2 in arr[i + 1:]:
+            for (x, y) in get_valid_antinodes_many(loc1, loc2):
+                antinodes2.add((x, y))
+
+part_two = len(antinodes2)
 print(f"Part two = {part_two}")
