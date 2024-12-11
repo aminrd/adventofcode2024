140d136d8800ec4e4239aea05ea04b1a8e48a25e
Amin <amin.rad.1371@gmail.com>
2024-12-10 21:25:01 -0800

    Day 11 part two

--- day11.py
+++ day11.py
@@ -1,5 +1,6 @@
 import sys
 from tqdm import tqdm
+from collections import defaultdict
 
 DEBUG = sys.gettrace() is not None
 input_file = "inputs/test.txt" if DEBUG else "./inputs/day11.txt"
@@ -24,6 +25,21 @@ def evolve(num, output: list):
     output.append(int(n2))
 
 
+def evolve2(key: int, value: int, output_counter: dict):
+    if key == 0:
+        output_counter[1] += value
+        return
+
+    s = str(key)
+    if len(s) % 2 == 1:
+        output_counter[key * 2024] += value
+        return
+
+    n1, n2 = s[:len(s) // 2], s[len(s) // 2:]
+    output_counter[int(n1)] += value
+    output_counter[int(n2)] += value
+
+
 number_of_iterations = 25
 for _ in tqdm(range(number_of_iterations)):
     output = []
@@ -34,9 +50,22 @@ for _ in tqdm(range(number_of_iterations)):
     if len(arr) >= 10 ** 8:
         raise Exception("Growing too much!")
 
-
 part_one = len(arr)
 print(f"Part one = {part_one}")
 
-part_two = None
+arr = list(map(int, lines[0].split()))
+
+cnt = defaultdict(int)
+for num in arr:
+    cnt[num] = 1
+
+for _ in tqdm(range(75)):
+    new_cnt = defaultdict(int)
+    for k, v in cnt.items():
+        evolve2(k, v, new_cnt)
+
+    del cnt
+    cnt = new_cnt
+
+part_two = sum(cnt.values())
 print(f"Part two = {part_two}")
