24db89ebf60a0ced1549472924d25ffdd9f37f2d
Amin <amin.rad.1371@gmail.com>
2024-12-18 08:57:32 -0800

    Day 16 part two

--- day16.py
+++ day16.py
@@ -1,5 +1,6 @@
 import sys
 import heapq
+from collections import defaultdict
 
 DEBUG = sys.gettrace() is not None
 input_file = "inputs/test.txt" if DEBUG else "./inputs/day16.txt"
@@ -43,8 +44,11 @@ for i, row in enumerate(grid):
 M, N = len(grid), len(grid[0])
 
 
-def move(position, direction):
-    return position[0] + direction[0], position[1] + direction[1]
+def move(position, direction, reverse=False):
+    if not reverse:
+        return position[0] + direction[0], position[1] + direction[1]
+    else:
+        return position[0] - direction[0], position[1] - direction[1]
 
 
 def valid(position):
@@ -78,5 +82,43 @@ def bfs(position=start, start_direction=RIGHT):
 part_one = bfs(start, RIGHT)
 print(f"Part one = {part_one}")
 
-part_two = None
+
+def manhattan(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+def minimum_remaining_cost(p):
+    return manhattan(p, target) + 1000 * (p[0] != target[0] and p[1] != target[1])
+
+
+def disjstra(position=start, start_direction=RIGHT, reverse=False):
+    dist = {(start, start_direction): 0}
+    # Construcing heap with (score, position, direction)
+
+    h = [(0, position, start_direction)]
+    while len(h) > 0:
+        score, p, d = heapq.heappop(h)
+        adj = move(p, d, reverse)
+        if valid(adj) and (adj, d) not in dist:
+            dist[(adj, d)] = score + 1
+            heapq.heappush(h, (score + 1, adj, d))
+
+        for d_new in (rotate_right[d], rotate_left[d]):
+            if (p, d_new) not in dist:
+                heapq.heappush(h, (score + 1000, p, d_new))
+                dist[(p, d_new)] = score + 1000
+
+    return dist
+
+
+from_source = disjstra(start, RIGHT)
+from_target = disjstra(target, UP, reverse=True)
+
+good_cells = {start, target}
+
+for k,v in from_source.items():
+    if v + from_target.get(k, INF) <= part_one:
+        good_cells.add(k[0])
+
+part_two = len(good_cells)
 print(f"Part two = {part_two}")
