0885823240966ea7408c1d8a4382c9e2596d3841
Amin <amin.rad.1371@gmail.com>
2024-12-19 15:58:17 -0800

    Day 19 part two

--- day19.py
+++ day19.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 DEBUG = sys.gettrace() is not None
 input_file = "inputs/test.txt" if DEBUG else "./inputs/day19.txt"
@@ -18,15 +19,36 @@ def possible(s: str, i: int = 0) -> bool:
         return cache[s[i:]]
 
     cache[s[i:]] = False
-    for j in range(i+1, len(s)):
+    for j in range(i + 1, len(s)):
         if cache.get(s[i:j], False) and possible(s, j):
             cache[s[i:]] = True
             return True
 
     return False
 
+
 part_one = sum(possible(s) for s in lines[2:])
 print(f"Part one = {part_one}")
 
-part_two = None
+count = defaultdict(int)
+
+
+def count_ways(s: str, i: int = 0) -> int:
+    if i >= len(s) or not possible(s[i:]):
+        return 0
+
+    if s[i:] in count:
+        return count[s[i:]]
+
+    if s[i:] in patterns:
+        count[s[i:]] = 1
+
+    for j in range(i + 1, len(s)):
+        if s[i:j] in patterns:
+            count[s[i:]] += count_ways(s, j)
+
+    return count[s[i:]]
+
+
+part_two = sum(count_ways(s) for s in lines[2:])
 print(f"Part two = {part_two}")
